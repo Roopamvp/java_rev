b48c79b24ee336777331cdf4fa862eeafb7ad75f
Roopamvp <roopampardhi111@gmail.com>
2021-06-15 09:30:13 +0530

    Tenth commit

---
+++ HardCode.java
@@ -0,0 +1,13 @@
+public class HardCode
+{
+	public static void main(String args[])
+	{
+	int a=2;
+	int b=3;
+	int sum=a+b;
+	int sub=b-a;
+	int mul=a*b;
+	float div = a/b;
+	System.out.println(sum);
+	}
+}
