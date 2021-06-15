9425eaa72b56d7994128297edc32436f11e1f034
Roopamvp <roopampardhi111@gmail.com>
2021-06-15 09:35:04 +0530

    twelfth commit

---
+++ SwitchCase.java
@@ -0,0 +1,22 @@
+public class SwitchCase
+	{
+		public static void main(String args[])
+		{
+		int x =100;
+		switch(x)
+		{
+			case 10:
+				System.out.println("Ten");
+				break;
+			case 100:
+				System.out.println("Hundred");
+				break;
+			case 1000:
+				System.out.println("Thousand");
+				break;
+			default:
+				System.out.println("NA");
+				break;
+		}
+		}
+}
