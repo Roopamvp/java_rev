a9bef6d9d4cb3411062fd2b8313ddc660d38b89e
Roopamvp <roopampardhi111@gmail.com>
2021-06-15 09:26:38 +0530

    eighth commit

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class DoWhile
+{
+	public static void main(String args[])
+	{	
+		int x = 5;
+		do
+		{
+		System.out.println(x);
+		x++;
+		}
+		while(x<4);
+	}
+}
+		
